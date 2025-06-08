4be0e42052d3115e3971007378de94fa7347c9ab
vzeme <v.zemela@gmail.com>
2025-06-08 22:03:00 +0300

    creat task1 for lesson3

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+let str = "JavaScript primitive data types";
+let num = 18;
+let bool = true;
+let empty = null;
+let Undefined;
+
+console.log("String text: ", str);
+console.log("Number №: ", num);
+console.log("Boolean: ", bool);
+console.log("Empty Null: ", empty);
+console.log("Undefined: ", Undefined);
