c3adac127f563e7cf6840f5a3557deda3e6fc6a8
vzeme <v.zemela@gmail.com>
2025-06-15 12:12:47 +0300

    task3.js for ДЗ 6.1. JavaScript intro to >> functions

---
+++ homework6.1. JavaScript intro to functions/task3.js
@@ -0,0 +1,15 @@
+function checkOrder(available, ordered) {
+  if (ordered === 0) {
+    return "Your order is empty";
+  }
+
+  if (ordered > available) {
+    return "Your order is too large, we don’t have enough goods.";
+  }
+
+  return "Your order is accepted";
+}
+
+console.log(checkOrder(89, 0));
+console.log(checkOrder(79, 89));
+console.log(checkOrder(50, 20));
