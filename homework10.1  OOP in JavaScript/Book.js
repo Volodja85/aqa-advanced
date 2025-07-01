9fe533d99e9acd60379b4f1c12c1d8e49c6edb8a
vzeme <v.zemela@gmail.com>
2025-07-02 00:15:35 +0300

    OOP in JavaScript

---
+++ homework10.1  OOP in JavaScript/Book.js
@@ -0,0 +1,69 @@
+class Book {
+  constructor(name, author, year) {
+    this._name = name;
+    this._author = author;
+    this._year = year;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  set name(value) {
+    if (typeof value !== "string" || value.trim() === "") {
+      throw new Error("Назва не відповідає відповдному формату");
+    }
+    this._name = value;
+  }
+
+  set author(value) {
+    if (typeof value !== "string" || value.trim() === "") {
+      throw new Error("Введіть правильне ім'я автора");
+    }
+    this._author = value;
+  }
+
+  set year(value) {
+    if (typeof value !== "number" || value < 0 || !year.isInteger(value)) {
+      throw new Error("Невірний формат року");
+    }
+    this._year = value;
+  }
+
+  printInfo() {
+    console.log(
+      `Назва: ${this._name}, Автор: ${this._author}, Рік: ${this._year}`
+    );
+  }
+
+  static OldBooks(books) {
+    let oldBooks = books[0];
+
+    for (let book of books) {
+      if (book.year < oldBooks.year) {
+        oldBooks = book;
+      }
+    }
+
+    return oldBooks;
+  }
+}
+
+const book1 = new Book("Пеппі Довгапанчоха", "Астрід Ліндґрен", 1945);
+const book2 = new Book("Північне сяйво", "Філіп Пулман", 1995);
+const book3 = new Book("Кобзар", "Тарас Шевченко", 1840);
+
+book1.printInfo();
+book2.printInfo();
+book3.printInfo();
+
+//export default Book;
+module.exports = Book;
---
+++ homework10.1  OOP in JavaScript/task1.js
@@ -0,0 +1,17 @@
+const Book = require("./Book.js");
+const EBook = require("./EBook");
+
+const book11 = new Book("Пеппі Довгапанчоха_2", "Астрід Ліндґрен", 1945);
+const book21 = new Book("Північне сяйво_2", "Філіп Пулман", 1995);
+const ebook11 = new EBook("Кобзар_2", "Тарас Шевченко", 1840, "APG");
+book11.printInfo();
+book21.printInfo();
+ebook11.printInfo();
+
+const books = [book11, book21, ebook11];
+const oldest = Book.OldBooks(books);
+console.log("Hайдавніша книга за роком видання:");
+console.log(oldest);
+
+const newEbook = EBook.createEBook(book21, ".xlsx");
+newEbook.printInfo();
