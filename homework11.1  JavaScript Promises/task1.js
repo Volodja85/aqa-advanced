cdf85f56325e97c08c1a25436eb7f0fa07011931
vzeme <v.zemela@gmail.com>
2025-07-05 19:13:03 +0300

    Use setTimeout

---
+++ homework11.1  JavaScript Promises/task1.js
@@ -0,0 +1,8 @@
+function printTextDelay(text, delay) {
+  setTimeout(function () {
+    console.log(text);
+  }, delay);
+}
+
+const timerId = printTextDelay("Затримка на 2 секунди", 2000);
+clearTimeout(timerId);
