f3e9920590414d78e423fd21b61da0366b4a2056
vzeme <v.zemela@gmail.com>
2025-07-09 00:03:01 +0300

    виправлений pre-commit hook

--- homework9.1  Objects in JavaScript/task5.js
+++ homework9.1  Objects in JavaScript/task5.js
@@ -1,10 +1,8 @@
 const users = [
-  { name: "Jeck", email: "Jeck@Gmail.com", age: 18 },
-  { name: "Mark", email: "Mark@Gmail.com", age: 22 },
-  { name: "Jork", email: "Jork@Gmail.com", age: 44 },
+	{ name: 'Jeck', email: 'Jeck@Gmail.com', age: 18 },
+	{ name: 'Mark', email: 'Mark@Gmail.com', age: 22 },
+	{ name: 'Jork', email: 'Jork@Gmail.com', age: 44 },
 ];
 for (const { name, email, age } of users) {
-  console.log(
-    `User "${name}" is ${age} y.o. and has "${email}" email address.`
-  );
+	console.log(`User "${name}" is ${age} y.o. and has "${email}" email address.`);
 }
